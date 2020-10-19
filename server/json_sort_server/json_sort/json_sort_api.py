3814f2083ea9812c38f248daba9f902a7f70de5c
yangzongbing <MISS9958>
2020-10-19 20:08:13 +0800

    add json sort view

--- server/json_sort_server/json_sort/json_sort_api.py
+++ server/json_sort_server/json_sort/json_sort_api.py
@@ -1,6 +1,7 @@
 from flask import request, Blueprint
 from server.json_sort_server.json_sort.json_sort_process import json_sort
 from server.json_sort_server.json_sort.input_check import security_check
+import json
 
 JSON_SORT_API = Blueprint("json_sort_api", __name__)
 
@@ -8,9 +9,11 @@ JSON_SORT_API = Blueprint("json_sort_api", __name__)
 @JSON_SORT_API.route("/v1/bing/etl/jsonsort", methods=["POST"])
 def post_poi_mapping_data():
     request_data = request.get_json()
+    main = request.form.to_dict().get("main")
+    request_data = json.loads(main) if main else request_data
     check_result = security_check(request_data)
     if check_result.get("SecurityCheckResult") == "SUCCEED":
-        check_result["data"] = json_sort(request_data)
+        check_result["data"] = json.dumps(json_sort(request_data), indent=4, ensure_ascii=False)
         # check_result["data"] = json_sort_simple(request_data)
         return check_result
     else:
