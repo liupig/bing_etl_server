1f4317cdc409af4eb34d64bc6336c7d0bb146408
yangzongbing <MISS9958>
2020-10-19 20:34:51 +0800

    del unless html

--- server/json_sort_server/json_sort/json_sort_api.py
+++ server/json_sort_server/json_sort/json_sort_api.py
@@ -13,7 +13,7 @@ def post_poi_mapping_data():
     request_data = json.loads(main) if main else request_data
     check_result = security_check(request_data)
     if check_result.get("SecurityCheckResult") == "SUCCEED":
-        check_result["data"] = json.dumps(json_sort(request_data), indent=4, ensure_ascii=False)
+        check_result["data"] = json.dumps(json_sort(request_data), indent=4, ensure_ascii=False) if main else json_sort(request_data)
         # check_result["data"] = json_sort_simple(request_data)
         return check_result
     else:
