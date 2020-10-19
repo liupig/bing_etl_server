ba2eba916d333e5cbbbce51afe8edded46cc23dd
yangzongbing <MISS9958>
2020-10-19 16:22:09 +0800

    rewrite difflib

--- server/text_comparison_server/text_comparison/text_comparison_process.py
+++ server/text_comparison_server/text_comparison/text_comparison_process.py
@@ -1,15 +1,15 @@
-import difflib
 import json
 from server.json_sort_server.json_sort.json_sort_process import json_sort
+from server.text_comparison_server.text_comparison.difflib import HtmlDiff
 
 
 def comparison_process(main, sub):
     try:
         main_text = json.dumps(json_sort(json.loads(main)), indent=4, ensure_ascii=False).splitlines()
         sub_text = json.dumps(json_sort(json.loads(sub)), indent=4, ensure_ascii=False).splitlines()
-        html = difflib.HtmlDiff().make_file(main_text, sub_text).encode()
+        html = HtmlDiff().make_file(main_text, sub_text).encode()
         return html
     except:
         main_text = main.splitlines()
         sub_text = sub.splitlines()
-        return difflib.HtmlDiff().make_file(main_text, sub_text).encode()
+        return HtmlDiff().make_file(main_text, sub_text).encode()
