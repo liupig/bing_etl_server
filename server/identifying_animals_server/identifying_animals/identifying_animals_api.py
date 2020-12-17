a3d7a6144c2239e9973f455a4ebc66230eac2fd8
yangzongbing <yangzongbing>
2020-12-18 00:10:11 +0800

    add module

--- server/identifying_animals_server/identifying_animals/identifying_animals_api.py
+++ server/identifying_animals_server/identifying_animals/identifying_animals_api.py
@@ -1,7 +1,7 @@
 import os
 from flask import Blueprint, render_template, request
 from werkzeug.utils import secure_filename
-# from server.identifying_animals_server.identifying_animals.identification_processing import evaluate_one_image
+from server.identifying_animals_server.identifying_animals.identification_processing import evaluate_one_image
 
 IDENTIFYING_ANIMALS_API = Blueprint("identifying_animals_api", __name__)
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
@@ -18,9 +18,7 @@ def post_poi_mapping_data():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join("./static/images/", filename))
-            print(filename)
-            result = {}
-            # result = evaluate_one_image(filename)
+            result = evaluate_one_image(filename)
             return render_template('upload_ok.html', result=result, imagename=filename)
 
         return render_template('upload_ok.html', result="耗子尾汁", imagename="error.jpg")
