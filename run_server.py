4fb41837b4d024944a42d73281388580fb681670
yangzongbing <yangzongbing>
2020-12-18 00:07:28 +0800

    add distinguishing animals

--- run_server.py
+++ run_server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from server.json_sort_server.json_sort.json_sort_api import JSON_SORT_API, JSON_SORT_V2_API
 from server.json_string_server.json_string.json_string_api import JSON_STRING_API
 from server.text_comparison_server.text_comparison.text_comparison_api import TEXT_COMPARISON_API
+from server.identifying_animals_server.identifying_animals.identifying_animals_api import IDENTIFYING_ANIMALS_API
 from server.index_server.index_view import TEXT_COMPARISON_INDEX, JSON_SORT_INDEX, JSON_STRING_INDEX
 
 APP = Flask(__name__)
@@ -9,6 +10,7 @@ APP.register_blueprint(JSON_SORT_API)
 APP.register_blueprint(JSON_STRING_API)
 APP.register_blueprint(JSON_SORT_V2_API)
 APP.register_blueprint(TEXT_COMPARISON_API)
+APP.register_blueprint(IDENTIFYING_ANIMALS_API)
 APP.register_blueprint(TEXT_COMPARISON_INDEX)
 APP.register_blueprint(JSON_SORT_INDEX)
 APP.register_blueprint(JSON_STRING_INDEX)
