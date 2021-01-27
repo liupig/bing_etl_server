eabba032bc5002f36c182af0a4a6220eb3f24efe
yangzongbing <MISS9958>
2021-01-27 15:54:07 +0800

    add statutory Holiday

--- run_server.py
+++ run_server.py
@@ -3,6 +3,7 @@ from server.json_sort_server.json_sort.json_sort_api import JSON_SORT_API
 from server.json_string_server.json_string.json_string_api import JSON_STRING_API
 from server.text_comparison_server.text_comparison.text_comparison_api import TEXT_COMPARISON_API
 from server.identifying_animals_server.identifying_animals.identifying_animals_api import IDENTIFYING_ANIMALS_API
+from server.statutory_holiday_server.statutory_holiday.statutory_holiday_api import STATUTORY_HOLIDAY_API
 from server.geohash_server.geohash.geo_api import GEO_API
 from server.index_server.index_view import SERVER_INDEX
 
@@ -12,6 +13,7 @@ APP.register_blueprint(JSON_SORT_API)
 APP.register_blueprint(JSON_STRING_API)
 APP.register_blueprint(TEXT_COMPARISON_API)
 APP.register_blueprint(IDENTIFYING_ANIMALS_API)
+APP.register_blueprint(STATUTORY_HOLIDAY_API)
 APP.register_blueprint(GEO_API)
 APP.register_blueprint(SERVER_INDEX)
 
---
+++ server/statutory_holiday_server/statutory_holiday/statutory_holiday_api.py
@@ -0,0 +1,25 @@
+from flask import request, Blueprint
+from server.statutory_holiday_server.statutory_holiday.statutory_holiday_process import get_statutory_holiday_info, \
+    statutory_holiday_info_to_csv
+import json
+import datetime
+import pandas as pd
+
+STATUTORY_HOLIDAY_PATH = "./server/statutory_holiday_server/statutory_holiday/"
+
+STATUTORY_HOLIDAY_API = Blueprint("statutory_holiday_api", __name__)
+STATUTORY_HOLIDAY_INFO = {}
+
+
+@STATUTORY_HOLIDAY_API.route("/v1/bing/etl/statutoryHoliday", methods=["GET"])
+def get_statutory_holiday():
+    year = datetime.datetime.now().year
+    df = pd.read_csv(STATUTORY_HOLIDAY_PATH + "national_holidays.csv", encoding="utf8")
+
+    df = df.loc[df["year"] == year]
+    if df.empty:
+        statutory_holiday_info_to_csv()
+
+    df = pd.read_csv(STATUTORY_HOLIDAY_PATH + "national_holidays.csv", encoding="utf8")
+    result = pd.DataFrame(df, columns=["name", "startDate", "endDate"]).to_dict(orient="records")
+    return {"result": result, "status": "ok"}
