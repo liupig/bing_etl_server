3fd612b7b4cc4e93c42f17365112b634da9346ba
yangzongbing <MISS9958>
2021-03-30 16:55:21 +0800

    del identifying_animals_api

--- run_server.py
+++ run_server.py
@@ -2,7 +2,6 @@ from flask import Flask
 from server.json_sort_server.json_sort.json_sort_api import JSON_SORT_API
 from server.json_string_server.json_string.json_string_api import JSON_STRING_API
 from server.text_comparison_server.text_comparison.text_comparison_api import TEXT_COMPARISON_API
-from server.identifying_animals_server.identifying_animals.identifying_animals_api import IDENTIFYING_ANIMALS_API
 from server.statutory_holiday_server.statutory_holiday.statutory_holiday_api import STATUTORY_HOLIDAY_API
 from server.geohash_server.geohash.geo_api import GEO_API
 from server.index_server.index_view import SERVER_INDEX
@@ -12,7 +11,6 @@ APP.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 APP.register_blueprint(JSON_SORT_API)
 APP.register_blueprint(JSON_STRING_API)
 APP.register_blueprint(TEXT_COMPARISON_API)
-APP.register_blueprint(IDENTIFYING_ANIMALS_API)
 APP.register_blueprint(STATUTORY_HOLIDAY_API)
 APP.register_blueprint(GEO_API)
 APP.register_blueprint(SERVER_INDEX)
